cabe40e2db4d2085f1fbafce0f68ca4f16c6d435
Abhishek kumar <73441904+Abhisheknov5@users.noreply.github.com>
2023-01-30 18:29:52 +0530

    Update 6 Number is positive or negative.js

--- Js Part-3/6 Number is positive or negative.js
+++ Js Part-3/6 Number is positive or negative.js
@@ -3,17 +3,14 @@ console.log("6 Check if a Number is Positive, Negative,or Zero");
 
 const number = parseInt(prompt("Enter a number: "));
 
-// check if number is greater than 0
 if (number > 0) {
     console.log("The number is positive");
 }
 
-// check if number is 0
 else if (number == 0) {
   console.log("The number is zero");
 }
 
-// if number is less than 0
 else {
      console.log("The number is negative");
 }
